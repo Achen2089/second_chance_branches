0d51fa8ba83516ac0343861f67703e6d5f97ecb7
Anthony Chen <anthonychen2089@gmail.com>
2020-04-20 12:29:06 -0700

    First commit

---
+++ quiz.js
@@ -0,0 +1,130 @@
+(function() 
+ {
+  var allQuestions = [{
+    question: "Do you have more than two felonies?",
+    options: ["Yes", "No"],
+    answer: 2
+  }, {
+    question: "Have you committed any of the listed crimes?",
+    options: ["Murder", "Rape", "None"],
+    answer: 3
+  }, {
+    question: "Was your last convication more than ten years from now?",
+    options: ["Yes", "No"],
+    answer: 1
+  },{
+    question: "What was the state in you committed?",
+    options: ["NY", "WA"],
+    answer: 0
+  }];
+  
+  var quesCounter = 0;
+  var selectOptions = [];
+  var quizSpace = $('#quiz');
+    
+  nextQuestion();
+    
+  $('#next').click(function () 
+    {
+        chooseOption();
+        if (isNaN(selectOptions[quesCounter])) 
+        {
+            alert('Please select an option !');
+        } 
+        else 
+        {
+          quesCounter++;
+          nextQuestion();
+        }
+    });
+  
+  $('#prev').click(function () 
+    {
+        chooseOption();
+        quesCounter--;
+        nextQuestion();
+    });
+  
+  function createElement(index) 
+    {
+        var element = $('<div>',{id: 'question'});
+        var header = $('<h2>Question No. ' + (index + 1) + ' :</h2>');
+        element.append(header);
+
+        var question = $('<p>').append(allQuestions[index].question);
+        element.append(question);
+
+        var radio = radioButtons(index);
+        element.append(radio);
+
+        return element;
+    }
+  
+  function radioButtons(index) 
+    {
+        var radioItems = $('<ul>');
+        var item;
+        var input = '';
+        for (var i = 0; i < allQuestions[index].options.length; i++) {
+          item = $('<li>');
+          input = '<input type="radio" name="answer" value=' + i + ' />';
+          input += allQuestions[index].options[i];
+          item.append(input);
+          radioItems.append(item);
+        }
+        return radioItems;
+  }
+  
+  function chooseOption() 
+    {
+        selectOptions[quesCounter] = +$('input[name="answer"]:checked').val();
+    }
+   
+  function nextQuestion() 
+    {
+        quizSpace.fadeOut(function() 
+            {
+              $('#question').remove();
+              if(quesCounter < allQuestions.length)
+                {
+                    var nextQuestion = createElement(quesCounter);
+                    quizSpace.append(nextQuestion).fadeIn();
+                    if (!(isNaN(selectOptions[quesCounter]))) 
+                    {
+                      $('input[value='+selectOptions[quesCounter]+']').prop('checked', true);
+                    }
+                    if(quesCounter === 1)
+                    {
+                      $('#prev').show();
+                    } 
+                    else if(quesCounter === 0)
+                    {
+                      $('#prev').hide();
+                      $('#next').show();
+                    }
+                }
+              else 
+                {
+                    var scoreRslt = displayResult();
+                    quizSpace.append(scoreRslt).fadeIn();
+                    $('#next').hide();
+                    $('#prev').hide();
+                }
+        });
+    }
+  
+  function displayResult() 
+    {
+        var score = $('<p>',{id: 'question'});
+        // var correct = 0;
+        // for (var i = 0; i < selectOptions.length; i++) 
+        // {
+        //   if (selectOptions[i] === allQuestions[i].answer) 
+        //   {
+        //     correct++;
+        //   }
+        // }
+        score.append('You are elligible for expungement');
+        return score;
+  }
+})();
